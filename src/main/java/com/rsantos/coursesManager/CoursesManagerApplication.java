2f96405042bd3f9f38e6ec031d2c356f8ad12ef5
Rodrigo Santos <rodrigostsbc@gmail.com>
2021-04-08 12:34:53 -0300

    First commit: Project struture created

---
+++ src/main/java/com/rsantos/coursesManager/CoursesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.rsantos.coursesManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesManagerApplication.class, args);
+	}
+
+}
